9eee86ea0694d0570793be86c5d3e8b8b2d119f8
pfigueroa82 <figueroap82@gmail.com>
2023-07-25 15:14:11 -0600

    version 1

--- src/gith/Gith.java
+++ src/gith/Gith.java
@@ -4,6 +4,8 @@
  */
 package gith;
 
+
+
 /**
  *
  * @author pfigueroa
@@ -14,7 +16,7 @@ public class Gith {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("hola mundo");
     }
     
 }
