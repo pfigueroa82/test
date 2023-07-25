fac60b0f1448d01d4eae10316a5108f159c0e0ea
pfigueroa82 <figueroap82@gmail.com>
2023-07-25 15:17:02 -0600

    version 2

--- src/gith/Gith.java
+++ src/gith/Gith.java
@@ -16,7 +16,7 @@ public class Gith {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("hola mundo");
+        System.out.print("hola mundo cambios");
     }
     
 }
